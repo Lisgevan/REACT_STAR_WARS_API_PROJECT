430d40793ef7123e8c05a15abce9c550f1d964b8
Lisgevan <episimo@hotmail.com>
2025-04-19 08:35:16 +0300

    updated to newer versions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
+import { createRoot } from "react-dom/client";
 
 // import App from "./App";
 import Canvas from "./canvas";
@@ -11,13 +12,22 @@ import "./css/starwarsintro.css";
 import "./css/App.css";
 import "./css/lightSaber.css";
 
-ReactDOM.render(
+// ReactDOM.render(
+// 	<React.StrictMode>
+// 		{/* <App /> */}
+// 		<Canvas />
+// 		<Main />
+// 	</React.StrictMode>,
+// 	document.getElementById("root")
+// );
+
+const root = createRoot(document.getElementById("root"));
+root.render(
 	<React.StrictMode>
 		{/* <App /> */}
 		<Canvas />
 		<Main />
-	</React.StrictMode>,
-	document.getElementById("root")
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
